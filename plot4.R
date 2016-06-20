b72e75bb59c9ebcbb1d810d7a09c6b1e5f501045
ryanfriederich <ryan.d.friederich@gmail.com>
2016-06-19 21:48:16 -0600

    Committing revised plot3.png and plot4.png

--- plot4.R
+++ plot4.R
@@ -24,8 +24,8 @@ with(consumption2, {
              xlab = "", ylab = "Energy sub metering")
         lines(DateTime, Sub_metering_2, col = "red")
         lines(DateTime, Sub_metering_3, col = "blue")
-        legend("topright", lty = "solid", col = c("black", "red", "blue"), 
-               legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
+        legend("topright", lty = "solid", col = c("black", "red", "blue"), bty = "n", 
+               legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
         plot(DateTime, Global_reactive_power, type = "l",
              xlab = "datetime", ylab = "Global_reactive_power")
 })
